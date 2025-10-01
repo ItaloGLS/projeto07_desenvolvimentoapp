93ff47ba23ce373a96807f6d9d8788d9834b7ecd
ItaloGLS <pmmgcj@gmail.com>
2025-10-01 19:17:33 -0300

    Subindo Projeto

---
+++ app/src/main/java/com/example/nvvm_module/MainActivity.kt
@@ -0,0 +1,24 @@
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.nvvm_module.ui.theme.Nvvm_moduleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Nvvm_moduleTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TasksScreen()
+                }
+            }
+        }
+    }
+}
